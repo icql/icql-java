436314184ecfd2ae0b7235707835d01daf3b6475
icql <chenqinglin@hnu.edu.cn>
2020-07-05 17:08:15 +0800

    增加pdf合并工具类

---
+++ util/src/main/java/work/icql/util/Application.java
@@ -0,0 +1,10 @@
+package work.icql.util;
+
+import work.icql.util.pdf.PdfMergeUtils;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        PdfMergeUtils.merge("C:\\Users\\icql\\Desktop\\test", "C:\\Users\\icql\\Desktop\\test");
+    }
+}
