fe2cf20d15f64ada03224896d6227254fd94c87e
icql <chenqinglin@hnu.edu.cn>
2020-09-30 17:45:00 +0800

    更新注释

--- algorithm/src/main/java/work/icql/java/algorithm/sort/isN/CountingSort.java
+++ algorithm/src/main/java/work/icql/java/algorithm/sort/isN/CountingSort.java
@@ -2,10 +2,9 @@ package work.icql.java.algorithm.sort.isN;
 
 /**
  * 计数排序：
- * 1）稳定排序
- * 2）算法思想
- *
- * 3）平均时间复杂度：O(n)，平均空间复杂度：O(n)
+ * 1）算法思想
+ * 当所需排序的数据范围不大且有限，可以在桶排序的基础上，相等的数据放在一个桶
+ * 2）平均时间复杂度：O(n)
  */
 public class CountingSort {
 
--- algorithm/src/main/java/work/icql/java/algorithm/sort/isN/RadixSort.java
+++ algorithm/src/main/java/work/icql/java/algorithm/sort/isN/RadixSort.java
@@ -2,10 +2,9 @@ package work.icql.java.algorithm.sort.isN;
 
 /**
  * 基数排序：
- * 1）稳定排序
- * 2）算法思想
- * <p>
- * 3）平均时间复杂度：O(n)，平均空间复杂度：O(n)
+ * 1）算法思想
+ * 比较数据的每一位，比如对手机号进行排序，可以先比较第一位，再比较第二位，以此类推
+ * 2）平均时间复杂度：O(n)
  */
 public class RadixSort {
 
