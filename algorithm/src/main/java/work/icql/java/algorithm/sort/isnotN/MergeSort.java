88accc28350011a6ffcfb4bcce27838b964bd94e
icql <chenqinglin@hnu.edu.cn>
2021-03-31 23:56:03 +0800

    fix: 归并排序

--- algorithm/src/main/java/work/icql/java/algorithm/sort/isnotN/MergeSort.java
+++ algorithm/src/main/java/work/icql/java/algorithm/sort/isnotN/MergeSort.java
@@ -34,23 +34,33 @@ public class MergeSort {
 
     private static void merge(int[] data, int left, int right, int mid) {
         //临时数组，用来保存当前用以归并的数据
-        int[] aux = Arrays.copyOfRange(data, left, right + 1);
+        int[] arr = Arrays.copyOfRange(data, left, right + 1);
         int leftIndex = 0;
         int leftIndexEnd = mid - left;
         int rightIndex = leftIndexEnd + 1;
-        int rightIndexEnd = aux.length - 1;
+        int rightIndexEnd = arr.length - 1;
         //遍历重新设置 原数组 [left,right] 的值使之有序
         for (int k = left; k <= right; k++) {
+            //左边元素已经处理完
+            if (leftIndex > leftIndexEnd) {
+                data[k] = arr[rightIndex];
+                rightIndex++;
+                continue;
+            }
             //右边元素已经处理完
-            //如果左边所指元素 <= 右边所指元素
-            if (rightIndex > rightIndexEnd || aux[leftIndex] <= aux[rightIndex]) {
-                data[k] = aux[leftIndex];
+            if (rightIndex > rightIndexEnd) {
+                data[k] = arr[leftIndex];
                 leftIndex++;
                 continue;
             }
-            //左边元素已经处理完
+            //如果左边所指元素 <= 右边所指元素
+            if (arr[leftIndex] <= arr[rightIndex]) {
+                data[k] = arr[leftIndex];
+                leftIndex++;
+                continue;
+            }
             //如果左边所指元素 > 右边所指元素
-            data[k] = aux[rightIndex];
+            data[k] = arr[rightIndex];
             rightIndex++;
         }
     }
