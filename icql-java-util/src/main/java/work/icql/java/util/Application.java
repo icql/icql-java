f0687f9e29a02314bf4b410143c251227fa114c6
icql <icqlchen@qq.com>
2024-08-18 18:02:48 +0800

    doc: 更新文档

--- icql-java-util/src/main/java/work/icql/java/util/Application.java
+++ icql-java-util/src/main/java/work/icql/java/util/Application.java
@@ -2,9 +2,13 @@ package work.icql.java.util;
 
 import work.icql.java.util.pdf.PdfMergeUtils;
 
+import java.util.concurrent.locks.LockSupport;
+
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        PdfMergeUtils.merge("C:\\Users\\Casstime\\Desktop\\test", "C:\\Users\\Casstime\\Desktop\\test11");
+
+        LockSupport.parkUntil(System.currentTimeMillis()+1000*60*10);
+//        PdfMergeUtils.merge("C:\\Users\\Casstime\\Desktop\\test", "C:\\Users\\Casstime\\Desktop\\test11");
     }
 }
